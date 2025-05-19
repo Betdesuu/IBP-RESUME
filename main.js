6fe51721354fc63603bbd9ea0f1f9c7452e2bf54
Betdesuu <163589053+Betdesuu@users.noreply.github.com>
2025-05-19 11:57:43 +0300

    last update

--- main.js
+++ main.js
@@ -1,37 +1,33 @@
-/*==================== MENU SHOW Y HIDDEN ====================*/
+
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close');
 
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
+
 if(navToggle){
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
 if(navClose){
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
 
-/*==================== REMOVE MENU MOBILE ====================*/
+
 
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
   const navMenu = document.getElementById('nav-menu')
-  // When we click on each nav__link, we remove the show-menu class
   navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
-/*==================== ACCORDION SKILLS ====================*/
+
 const skillsContent = document.getElementsByClassName('skills__content'),
       skillsHeader = document.querySelectorAll('.skills__header')
 
@@ -51,7 +47,7 @@ skillsHeader.forEach((el) => {
 })
 
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
 const section = document.querySelectorAll('section[id]')
 function scrollActive(){
     const scrollY = window.pageYOffset
@@ -70,15 +66,14 @@ function scrollActive(){
     })
 }
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
 function scrollHeader(){
     const nav = document.getElementById('header')
-    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
     if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*==================== SHOW SCROLL UP ====================*/ 
+
 function scrollUp(){
     const scrollUp = document.getElementById('scroll-up');
     if(this.scrollY >= 560)
@@ -90,12 +85,12 @@ function scrollUp(){
 window.addEventListener('scroll', scrollUp)
 
 
-/*==================== DARK LIGHT THEME ====================*/ 
+
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'bx-sun'
 
-// Previously selected topic (if user selected)
+// Previously selected topic
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
@@ -105,7 +100,6 @@ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-mo
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
   themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
